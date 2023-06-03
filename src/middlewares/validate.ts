081266fc1e42b7cdbbee58a8b0b86b0950a96a35
Rohan Doshi <rohan.doshi02@gmail.com>
2023-06-03 20:34:35 +0530

    Validation Changes

--- src/middlewares/validate.ts
+++ src/middlewares/validate.ts
@@ -15,7 +15,7 @@ const validate =
       const errorMessage = error.details
         .map((details) => details.message)
         .join(", ");
-      return next(new ApiError(httpStatus.BAD_REQUEST, errorMessage));
+      return res.status(httpStatus.BAD_REQUEST).json({ error: errorMessage });
     }
     Object.assign(req, value);
     return next();
