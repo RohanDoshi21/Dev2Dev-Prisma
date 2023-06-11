7f045e3ff2f098ddf9f614ab9fa438a9955af814
Rohan Doshi <rohan.doshi02@gmail.com>
2023-06-11 17:42:08 +0530

    Avatar Ui

--- src/utils/avatar.ts
+++ src/utils/avatar.ts
@@ -1,11 +1,9 @@
 import { Canvas } from "canvas";
 
 export const generateAvatar = async (initials: string) => {
-  const canvas = new Canvas(64, 64);
+  const canvas = new Canvas(128, 128);
   const ctx = canvas.getContext("2d");
 
-  // Randomize the fill style and fill color
-  const fillStyles = ["solid", "gradient"];
   const fillColors = [
     "#FF0000",
     "#00FF00",
@@ -13,27 +11,23 @@ export const generateAvatar = async (initials: string) => {
     "#FFFF00",
     "#00FFFF",
     "#FF00FF",
+    "#521477",
+    "#C74B50",
+    "#4E9F3D",
+    "#1597BB",
+    "#03C4A1",
+    "#FA7D09",
+    "#FCCB0B",
+    "#FF4301",
+    "#FFD700",
+    "#2A2438",
   ];
-  const randomFillStyle =
-    fillStyles[Math.floor(Math.random() * fillStyles.length)];
+
   const randomFillColor =
     fillColors[Math.floor(Math.random() * fillColors.length)];
 
-  if (randomFillStyle === "solid") {
-    ctx.fillStyle = randomFillColor;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-  } else if (randomFillStyle === "gradient") {
-    const gradient = ctx.createLinearGradient(
-      0,
-      0,
-      canvas.width,
-      canvas.height
-    );
-    gradient.addColorStop(0, randomFillColor);
-    gradient.addColorStop(1, "#FFFFFF");
-    ctx.fillStyle = gradient;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-  }
+  ctx.fillStyle = randomFillColor;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   ctx.fillStyle = "#FFF";
   ctx.font = "bold 40px Arial";
